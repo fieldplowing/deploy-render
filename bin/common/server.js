cb2b08b67313c2ad59d2884f83c0cb97249a1fa1
fieldplowing <fieldplowing@gmail.com>
2022-09-26 13:17:01 +0900

    OpenAPI の定義にないパスのバリデーションをしない。

--- bin/common/server.js
+++ bin/common/server.js
@@ -50,11 +50,12 @@ class ExpressServer {
         const apiSpec = path_1.default.join(__dirname, 'api.yml');
         const validateResponses = !!(process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION &&
             process.env.OPENAPI_ENABLE_RESPONSE_VALIDATION.toLowerCase() === 'true');
-        app.use(process.env.OPENAPI_SPEC || '/spec', express_1.default.static(apiSpec));
+        // app.use(process.env.OPENAPI_SPEC || '/spec', express.static(apiSpec));
         app.use(OpenApiValidator.middleware({
             apiSpec,
             validateResponses,
-            ignorePaths: /.*\/spec(\/|$)/,
+            ignorePaths: (p) => !p.startsWith("/api/v1"),
+            // ignorePaths: /.*\/spec(\/|$)/,
         }));
     }
     router(routes) {
